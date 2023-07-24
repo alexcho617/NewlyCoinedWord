32c9b3f38bf39e3235a2043e6301d5046f8e8a23
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-24 11:11:14 +0900

    [Test] Tested user interaction function on image view.

--- NewlyCoinedWord/DeliveryViewController.swift
+++ NewlyCoinedWord/DeliveryViewController.swift
@@ -9,16 +9,26 @@ import UIKit
 
 class DeliveryViewController: UIViewController {
 
+    @IBOutlet var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("viewDidLoad")
-        print("gitTest")
-        print("gitTest")
-        print("gitTest")
+        print(image.isUserInteractionEnabled)
+        image.isUserInteractionEnabled = true
+        print(image.isUserInteractionEnabled)
+    }
+
+    @IBAction func image(_ sender: UITapGestureRecognizer) {
+        print("image")
     }
     
-
-
+    @IBAction func innerImage(_ sender: UITapGestureRecognizer) {
+        print("inner Image")
+    }
+    @IBAction func superView(_ sender: UITapGestureRecognizer) {
+        print("Superview")
+    }
+    
 }
